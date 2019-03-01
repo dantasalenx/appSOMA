139f8a7faffa27e9b57839ae0ff5271dc198674f
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-02-28 21:43:33 -0400

    calc android

---
+++ Textando.Android/obj/Debug/android/src/md5a1221f55afe8f5c2d06c32caaa4ce5ed/MainActivity.java
@@ -0,0 +1,47 @@
+package md5a1221f55afe8f5c2d06c32caaa4ce5ed;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Textando.Droid.MainActivity, Textando.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Textando.Droid.MainActivity, Textando.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ Textando/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Textando
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Soma(object sender, System.EventArgs e)
+        {
+            int resultado = 0;
+
+            resultado = Int32.Parse(campo1.Text) + Int32.Parse(campo2.Text);
+            campo3.Text = resultado.ToString();
+
+        }
+    }
+}
